dfecc6e7260f29f827233953ae32772010b13341
Yorevi86 <99880204+Yorevi86@users.noreply.github.com>
2023-03-02 17:17:24 +0100

    Inclusión de proyecto

---
+++ src/app/_services/account.service.ts
@@ -0,0 +1,90 @@
+﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AccountService {
+  private userSubject: BehaviorSubject<User | null>;
+  public user: Observable<User | null>;
+
+  constructor(private router: Router, private http: HttpClient) {
+    this.userSubject = new BehaviorSubject(
+      JSON.parse(localStorage.getItem('user')!)
+    );
+    this.user = this.userSubject.asObservable();
+  }
+
+  public get userValue() {
+    return this.userSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post<User>(`${environment.apiUrl}/users/authenticate`, {
+        username,
+        password,
+      })
+      .pipe(
+        map((user) => {
+          // Guarda los detalles del usuario y el json web token en el almacenamiento local para mantener
+          // al usuario conectado entre las actualizaciones de la página
+          localStorage.setItem('user', JSON.stringify(user));
+          this.userSubject.next(user);
+          return user;
+        })
+      );
+  }
+
+  logout() {
+    // Elimina los datos locales almacenados poniéndolos a null
+    localStorage.removeItem('user');
+    this.userSubject.next(null);
+    this.router.navigate(['/account/login']);
+  }
+
+  register(user: User) {
+    return this.http.post(`${environment.apiUrl}/users/register`, user);
+  }
+
+  getAll() {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+  getById(id: string) {
+    return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+  }
+
+  update(id: string, params: any) {
+    return this.http.put(`${environment.apiUrl}/users/${id}`, params).pipe(
+      map((x) => {
+        // Se actualizan los datos almacenados del usuario conectado en caso de que se actualicen sus datos
+        if (id == this.userValue?.id) {
+          // Actualización de datos locales
+          const user = { ...this.userValue, ...params };
+          localStorage.setItem('user', JSON.stringify(user));
+
+          // Se publican los datos del usuario actualizados
+          this.userSubject.next(user);
+        }
+        return x;
+      })
+    );
+  }
+
+  delete(id: string) {
+    return this.http.delete(`${environment.apiUrl}/users/${id}`).pipe(
+      map((x) => {
+        // Desconexción automática en caso de que el usuario borre su propio usuario
+        if (id == this.userValue?.id) {
+          this.logout();
+        }
+        return x;
+      })
+    );
+  }
+}
---
+++ src/app/users/add-edit.component.ts
@@ -0,0 +1,94 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
+
+@Component({ templateUrl: 'add-edit.component.html' })
+export class AddEditComponent implements OnInit {
+  form!: FormGroup;
+  id?: string;
+  title!: string;
+  loading = false;
+  submitting = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+
+    // Reglas de validación del formulario
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      // Solo se solicita la contraseña en caso de añadir un usuario
+      password: [
+        '',
+        [Validators.minLength(6), ...(!this.id ? [Validators.required] : [])],
+      ],
+    });
+
+    this.title = 'Add User';
+    if (this.id) {
+      // Método de edición
+      this.title = 'Edit User';
+      this.loading = true;
+      this.accountService
+        .getById(this.id)
+        .pipe(first())
+        .subscribe((x) => {
+          this.form.patchValue(x);
+          this.loading = false;
+        });
+    }
+  }
+
+  // Get para recoger los campos del formulario de manera sencilla
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // Se reinician las alertas en el envío del formulario
+    this.alertService.clear();
+
+    // Se detiene aquí si el formulario no es válido
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.submitting = true;
+    this.saveUser()
+      .pipe(first())
+      .subscribe({
+        next: () => {
+          this.alertService.success('Usuario guardado', {
+            keepAfterRouteChange: true,
+          });
+          this.router.navigateByUrl('/users');
+        },
+        error: (error) => {
+          this.alertService.error(error);
+          this.submitting = false;
+        },
+      });
+  }
+
+  private saveUser() {
+    // Crear o actualizar el usuario basado en los parámetros introducidos
+    return this.id
+      ? this.accountService.update(this.id!, this.form.value)
+      : this.accountService.register(this.form.value);
+  }
+}
